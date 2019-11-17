f5140e04a04cb450a5396a783a192fc023e15745
Raynaldi1 <raynaldi1@student.umn.ac.id>
2019-11-17 19:43:53 +0700

    camera

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ export const environment = {
   firebase: { 
 
     //edous
-    apiKey: "AIzaSyAUm7pdPCLBJhPVgizZ38UeZbt-Vhdw8fQ",
-    authDomain: "proyekuas-25e57.firebaseapp.com",
-    databaseURL: "https://proyekuas-25e57.firebaseio.com",
-    projectId: "proyekuas-25e57",
-    storageBucket: "proyekuas-25e57.appspot.com",
-    messagingSenderId: "881821814775",
-    appId: "1:881821814775:web:66c7c6236c6f70719228f2",
-    measurementId: "G-BMG6CE071S"
+    // apiKey: "AIzaSyAUm7pdPCLBJhPVgizZ38UeZbt-Vhdw8fQ",
+    // authDomain: "proyekuas-25e57.firebaseapp.com",
+    // databaseURL: "https://proyekuas-25e57.firebaseio.com",
+    // projectId: "proyekuas-25e57",
+    // storageBucket: "proyekuas-25e57.appspot.com",
+    // messagingSenderId: "881821814775",
+    // appId: "1:881821814775:web:66c7c6236c6f70719228f2",
+    // measurementId: "G-BMG6CE071S"
 
     // fidel
     // apiKey: "AIzaSyAIAka1wLaOY-JuPLQ6V4yFoSVkZ5UBPAQ",
@@ -27,13 +27,13 @@ export const environment = {
     // measurementId: "G-ZETWP2YS01"
 
     // //gs
-    // apiKey: "AIzaSyB6WkozZBuIFZcVJXPrKpFI1bvFE-lGwLs",
-    // authDomain: "proyekuas-3fb74.firebaseapp.com",
-    // databaseURL: "https://proyekuas-3fb74.firebaseio.com",
-    // projectId: "proyekuas-3fb74",
-    // storageBucket: "proyekuas-3fb74.appspot.com",
-    // messagingSenderId: "467545500271",
-    // appId: "1:467545500271:web:e2264b798e9209a3903b9a"
+    apiKey: "AIzaSyB6WkozZBuIFZcVJXPrKpFI1bvFE-lGwLs",
+    authDomain: "proyekuas-3fb74.firebaseapp.com",
+    databaseURL: "https://proyekuas-3fb74.firebaseio.com",
+    projectId: "proyekuas-3fb74",
+    storageBucket: "proyekuas-3fb74.appspot.com",
+    messagingSenderId: "467545500271",
+    appId: "1:467545500271:web:e2264b798e9209a3903b9a" 
 
     //yo
     // apiKey: "AIzaSyBt5t_wRR5ScLsnvZ8cQkpdxEJUyIFwYvY",
