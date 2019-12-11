12e2ee6a795172ffea89e3dd45f91d0485d0ff83
Fidel <chubi.axe@gmail.com>
2019-12-11 21:48:27 +0700

    revisi 1

--- src/app/home/meeting/meeting.page.ts
+++ src/app/home/meeting/meeting.page.ts
@@ -14,15 +14,22 @@ export class MeetingPage{
   constructor(private socialSharing:SocialSharing, private geolocation:Geolocation) { }
 
   ShareLocation(){
+    this.socialSharing.canShareViaEmail().then(() => {
+      console.log("Bisa pake email");
+    }).catch((err) => {
+      console.log(err);
+    });
     this.geolocation.getCurrentPosition().then((position)=>{
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    var maplink = "http://www.google.com/maps/?q="+latitude+","+longitude;
-    this.socialSharing.share("i am here...","Location"," ",maplink).then((data)=>{
-       
-    },(err)=>{
-      alert(JSON.stringify(err));
-    })
+    console.log(latitude);
+    console.log(longitude);
+    var maplink = "https://www.google.com/maps/?q="+latitude+","+longitude;
+    this.socialSharing.shareViaEmail(maplink, 'location',['chubi.axe@gmail.com']).then(() => {
+      console.log("success");
+    }).catch((err) => {
+      console.log(err);
+    });
     },(err)=>{
       alert(JSON.stringify(err));
     })
