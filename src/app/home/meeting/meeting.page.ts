923ad78b32667b5cc1d7f715daa30fcbf8d16aaa
Fidel <chubi.axe@gmail.com>
2019-12-11 22:57:33 +0700

    revisi 2

--- src/app/home/meeting/meeting.page.ts
+++ src/app/home/meeting/meeting.page.ts
@@ -3,6 +3,10 @@ import { HomeService, Todo } from '../home.service';
 
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Plugins } from '@capacitor/core';
+
+const{Storage} = Plugins;
 
 @Component({
   selector: 'app-meeting',
@@ -10,28 +14,51 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
   styleUrls: ['./meeting.page.scss'],
 })
 export class MeetingPage{
+  imageURL: string;
+  index:string;
 
-  constructor(private socialSharing:SocialSharing, private geolocation:Geolocation) { }
+  todo: Todo= {
+    idd:null,
+    title: null,
+    deskripsi: null,
+    nmbahan: null,
+    takaran: null,
+    langkah: null,
+    takasaji: null,
+    waktusiap: null,
+    totalmasak: null,
+    createdAt: new Date().getTime(),
+    name: null,
+    date: null,
+    alamat: null,
+    phone: null,
+    time: null,
+    random: null,
+    username: null,
+    uid: null
+  }
+
+  constructor(private socialSharing:SocialSharing, private geolocation:Geolocation,private afstore:AngularFirestore) { }
 
   ShareLocation(){
-    this.socialSharing.canShareViaEmail().then(() => {
-      console.log("Bisa pake email");
-    }).catch((err) => {
-      console.log(err);
-    });
     this.geolocation.getCurrentPosition().then((position)=>{
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     console.log(latitude);
     console.log(longitude);
     var maplink = "https://www.google.com/maps/?q="+latitude+","+longitude;
-    this.socialSharing.shareViaEmail(maplink, 'location',['chubi.axe@gmail.com']).then(() => {
-      console.log("success");
-    }).catch((err) => {
-      console.log(err);
-    });
-    },(err)=>{
-      alert(JSON.stringify(err));
+    const idmap = Storage.get({key : 'idTl'})
+    const linkmap = maplink;
+    this.afstore.doc(`todos/${this.index}`).update({
+      random : linkmap
+    })
+    // this.socialSharing.shareViaEmail(maplink, 'location',['chubi.axe@gmail.com']).then(() => {
+    //   console.log("success");
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
+    // },(err)=>{
+    //   alert(JSON.stringify(err));
     })
   }
 
